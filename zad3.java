4bfb8e1b0499849b2f1a543d1fafd7f5716492c9
sebastianbakala <sebastianbakala@users.noreply.github.com>
2018-04-23 19:05:00 +0200

    Update zad3.java

--- zad3.java
+++ zad3.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class zad3 {
-    public static void main (String [] args) {
+public class zad3
+{
+    public static void main (String [] args)
+    {
         Boolean doYouWannaPlayAgain = false;
         int widthOfTheCompartment = 100;
-        do {
+        do
+        {
             Scanner userInput = new Scanner(System.in);
             Random generator = new Random();
             int magicNumber = generator.nextInt(widthOfTheCompartment + 1);                                      // + 1, aby zapewnić przedział <0, 200>
@@ -13,15 +16,15 @@ public class zad3 {
             System.out.println("" + "Wylosowana liczba jest z przedziału 0:" + widthOfTheCompartment);
             System.out.println("Zgadnij co to za liczba");
             System.out.println("" + "Podpowiedź: Liczba = " + magicNumber);
-            for (int attemptCounter = 0; ; attemptCounter++) {
+            for (int attemptCounter = 0; ; attemptCounter++)
+            {
                 int choice = userInput.nextInt();
-                if (choice < magicNumber) {
+                if (choice < magicNumber)
                     System.out.println("Podana liczba jest mniejsza niż szukana");
-                }
-                else if (choice > magicNumber) {
+                else if (choice > magicNumber)
                     System.out.println("Podana liczba jest większa niż szukana");
-                }
-                else {
+                else
+                {
                     System.out.println("" + "Trafione za " + (attemptCounter + 1) + " razem");
                     break;
                 }
